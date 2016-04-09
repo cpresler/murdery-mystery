06e3dcd76483e5c74f8ece19dc08a7419966108c
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2016-04-09 19:27:34 -0400

    Update getattr.py

--- getattr.py
+++ getattr.py
@@ -10,8 +10,10 @@ def get_attr_intent_handler(request):
 
   if attr == None:
     return alexa.create_response("Could not find an attr!")
-    card = alexa.create_card(title="GetRecipeIntent activated", subtitle=None,
-                           content="asked alexa to find a recipe using {}".format(attr))
-    
-  return alexa.create_response("Finding a recipe with the attr {}".format(attr),
-                               end_session=False, card_obj=card)
+
+  return alexa.create_response("Finding a recipe with the attr {}".format(attr), end_session=False)
+
+def hasAttr(attr):
+  # stuff
+  
+  return answer
