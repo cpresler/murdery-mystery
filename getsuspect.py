9dc6246584919d7e87371e87c089cbefd6cabeae
Christy Presler <cnpresler@gmail.com>
2016-04-09 18:57:07 -0400

    testing with get suspect

--- getsuspect.py
+++ getsuspect.py
@@ -6,13 +6,23 @@ def get_suspect_intent_handler(request):
     """
 
     # Get variables like userId, slots, intent name etc from the 'Request' object
-    suspect = request.slots["Suspect"] 
+    # suspect = request.slots["Suspect"] 
 
-    if suspect == None:
-        return alexa.create_response("Could not find a suspect!")
+    # if suspect == None:
+    #     return alexa.create_response("Could not find a suspect!")
 
-    card = alexa.create_card(title="GetRecipeIntent activated", subtitle=None,
-                             content="asked alexa to find a recipe using {}".format(suspect))
+    # card = alexa.create_card(title="GetRecipeIntent activated", subtitle=None,
+    #                          content="asked alexa to find a recipe using {}".format(suspect))
     
-    return alexa.create_response("Finding a recipe with the suspect {}".format(suspect),
-                                 end_session=False, card_obj=card)
+    # return alexa.create_response("Finding a recipe with the suspect {}".format(suspect),
+    #                              end_session=False, card_obj=card)
+
+
+    # Testing Code:
+    test = request.slots["test"] 
+
+    if test == None:
+        return alexa.create_response("Could not find a test phrase!")
+    
+    return alexa.create_response("Hello World this is my test {}".format(test),
+                                 end_session=False)
