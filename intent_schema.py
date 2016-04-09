35e88d169850f1d8f51e7e16d67c65f5d9787e1d
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2016-04-09 18:13:27 -0400

    Create intent_schema.py

---
+++ intent_schema.py
@@ -0,0 +1,21 @@
+{
+  "intents": [
+    {
+      "intent" : "GetSuspect",
+      "slots" : [
+        {
+          "name" : "Suspect",
+          "type" : "LIST_OF_SUSPECTS"
+        }
+      ]
+    },
+    {
+      "intent" : "GetAttr",
+      "slots" : [
+        {
+          "name" : "Attr",
+          "type" : "LIST_OF_ATTRS"
+        }]
+    }
+  ]
+}
