bc989bc47f9df68dfea5a43bf33dc0db700a834f
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2016-04-09 18:44:43 -0400

    Added beginning of a program!!

--- getattr.py
+++ getattr.py
@@ -1,1 +1,17 @@
 # Program for the GetAttr intent
+@alexa.intent_handler('GetAttr')
+def get_attr_intent_handler(request):
+  """
+  20 questions -- except not
+  """
+
+  # Get variables like userId, slots, intent name etc from the 'Request' object
+  attr = request.slots["Attr"] 
+
+  if attr == None:
+    return alexa.create_response("Could not find an attr!")
+    card = alexa.create_card(title="GetRecipeIntent activated", subtitle=None,
+                           content="asked alexa to find a recipe using {}".format(attr))
+    
+  return alexa.create_response("Finding a recipe with the attr {}".format(attr),
+                               end_session=False, card_obj=card)
