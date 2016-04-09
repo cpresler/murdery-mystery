d576fa5655a18c8e108df3dfa515f737981c1126
Christy Presler <cnpresler@gmail.com>
2016-04-09 18:12:14 -0400

    initial suspects list

---
+++ suspects.py
@@ -0,0 +1,56 @@
+suspects = {
+	"suspect1": {
+		"name": "",
+		"facial hair": "yes",
+		"scarf": "no",
+		"hat": "yes",
+		"old": "No",
+		"pet": "yes",
+		"glasses": "yes"
+	},
+	"suspect2": {
+		"name": "",
+		"facial hair": "no",
+		"scarf": "yes",
+		"hat": "no",
+		"old": "yes",
+		"pet": "yes",
+		"glasses": "no"
+	},
+	"suspect3": {
+		"name": "",
+		"facial hair": "yes",
+		"scarf": "no",
+		"hat": "yes",
+		"old": "yes",
+		"pet": "yes",
+		"glasses": "no"
+	},
+	"suspect4": {
+		"name": "",
+		"facial hair": "yes",
+		"scarf": "no",
+		"hat": "no",
+		"old": "yes",
+		"pet": "no",
+		"glasses": "yes"
+	},
+	"suspect5": {
+		"name": "",
+		"facial hair": "no",
+		"scarf": "no",
+		"hat": "yes",
+		"old": "no",
+		"pet": "no",
+		"glasses": "no"
+	},
+	"suspect6": {
+		"name": "",
+		"facial hair": "no",
+		"scarf": "yes",
+		"hat": "yes",
+		"old": "no",
+		"pet": "no",
+		"glasses": "yes"
+	}
+}
