eb49a4c23cdee2f93678efecfcb105617732d26c
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2016-04-09 18:40:50 -0400

    Added beginning of a program!!!!!!

--- getsuspect.py
+++ getsuspect.py
@@ -1,1 +1,18 @@
 # Program for the GetSuspect intent
+@alexa.intent_handler('GetSuspect')
+def get_suspect_intent_handler(request):
+    """
+    Whodunnit??
+    """
+
+    # Get variables like userId, slots, intent name etc from the 'Request' object
+    suspect = request.slots["Suspect"] 
+
+    if ingredient == None:
+        return alexa.create_response("Could not find an ingredient!")
+
+    card = alexa.create_card(title="GetRecipeIntent activated", subtitle=None,
+                             content="asked alexa to find a recipe using {}".format(ingredient))
+    
+    return alexa.create_response("Finding a recipe with the ingredient {}".format(ingredient),
+                                 end_session=False, card_obj=card)
