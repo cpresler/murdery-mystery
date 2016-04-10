e08876a5927d1c3a1a08eafc4a9ed285601d3de4
Dee Cater <dee@deecater.com>
2016-04-10 10:22:13 -0400

    turn counter and elif statements

--- getsuspect.py
+++ getsuspect.py
@@ -5,7 +5,7 @@ def get_suspect_intent_handler(request):
     Whodunnit??
     """
     # Get variables like userId, slots, intent name etc from the 'Request' object
-    suspect = request.slots["Suspect"] 
+    suspect = request.slots["Suspect"]
 
 
     if suspect == None:
@@ -13,6 +13,14 @@ def get_suspect_intent_handler(request):
 
     elif suspect == killer:
         return alexa.create_response("You are correct! Congratulations, you have saved the day. Well, not the murder victims day. Or the killer’s day.  But someone’s day was surely saved.", end_session=True)
+
+    elif susepct != killer and turn >1:
+        return alexa.create_response("Nope! You're wrong. Ask for a clue about the killer or guess the killer again.")
+        turn = turn -1
+
+    elif suspect != killer and turn = 1:
+        return alexa.create_response("Too bad so sad! You're wrong. You have one more guess to catch the killer.")
+        turn = turn - 1
 
     else:
         return alexa.create_response("You are wrong, the killer will now go free and the victim’s murder will never be solved. Way to ruin everything. ", end_session=True)
