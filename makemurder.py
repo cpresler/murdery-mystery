2c68495474b1890f06660a89025ef4a3642a7857
Dee Cater <dee@deecater.com>
2016-04-09 20:28:08 -0400

    Update makemurder.py

--- makemurder.py
+++ makemurder.py
@@ -1,9 +1,28 @@
 # Program for picking a killer, location, and weapon.
 
+#import random module, weapon list, location list, and suspects dictionaries
 import random
 import weaponlist
+import Place_List
+import suspects
 
+#use random module to select a random murder weapon
 murder_weapon = random.choice(weapon_list)
+
+#use random module to select a random murder location
+murder_location = random.choice(place_of_murdery)
+
+#create a suspected killers list
+suspected_killers = []
+
+#interate through the suspect dictionaries and append the 'name' values to the suspected killers list
+for suspect in suspects.keys():
+	suspected_killers.append(suspects[suspect]['name'])
+
+#use random module to select a random killer
+killer = random.choice(suspected_killers)
+
+
 
 
 
